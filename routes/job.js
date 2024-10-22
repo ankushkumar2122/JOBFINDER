6028ef41272058caa6997f37c8a400a94f61f6cb
Ankush kumar <ankushkumarsingh374@gmail.com>
2024-10-22 23:31:39 +0530

    privacy button working

--- routes/job.js
+++ routes/job.js
@@ -2,61 +2,47 @@ const express = require("express");
 const router = express.Router();
 const WrapAsync = require("../utils/WrapAsync.js");
 const Job = require("../models/job.js");
-const { isLoggedIn,isOwner,validatejob } = require("../middleware.js");
+const { isLoggedIn, isOwner, validatejob } = require("../middleware.js");
 
-const jobcontroller=require("../controllers/job.js");
+const jobcontroller = require("../controllers/job.js");
 
-
-
- 
-//index route & //create
-router.route("")
-.get( WrapAsync(jobcontroller.index ))
-.post( isLoggedIn, validatejob, WrapAsync(jobcontroller.createJob));
-
-
-//new job
-router.get("/new", isLoggedIn,jobcontroller.renderNewForm);
-
-//show route & //update & //delete
-router.route("/:id")
-.get( WrapAsync(jobcontroller.showjob))
-.put( isLoggedIn, isOwner,validatejob, WrapAsync(jobcontroller.updateJob))
-.delete( isLoggedIn,isOwner, WrapAsync(jobcontroller.destroyJob));
-
-
-
-
-// term and condition
+// Static routes should be defined first to avoid conflict with dynamic routes
+// Terms and Conditions
 router.get("/terms&condition", (req, res) => {
     res.render("jobs/Terms&Conditions.ejs");
 });
-//privacy and cookies
+
+// Privacy and Cookies
 router.get("/privacy&cookies", (req, res) => {
     res.render("jobs/privacyandcookies.ejs");
 });
-//contactus
+
+// Contact Us
 router.get("/contactus", (req, res) => {
     res.render("jobs/contactus.ejs");
 });
-//about us
+
+// About Us
 router.get("/aboutus", (req, res) => {
     res.render("jobs/aboutus.ejs");
 });
 
+// Routes for jobs
+// Index route & create
+router.route("")
+    .get(WrapAsync(jobcontroller.index))
+    .post(isLoggedIn, validatejob, WrapAsync(jobcontroller.createJob));
 
+// New job
+router.get("/new", isLoggedIn, jobcontroller.renderNewForm);
 
+// Dynamic routes for show, update, delete
+router.route("/:id")
+    .get(WrapAsync(jobcontroller.showjob))
+    .put(isLoggedIn, isOwner, validatejob, WrapAsync(jobcontroller.updateJob))
+    .delete(isLoggedIn, isOwner, WrapAsync(jobcontroller.destroyJob));
 
-
-
-
-//edit
-router.get("/:id/edit", isLoggedIn,isOwner, WrapAsync(jobcontroller.renderEditForm));
-
-
-
-
-
-
+// Edit job
+router.get("/:id/edit", isLoggedIn, isOwner, WrapAsync(jobcontroller.renderEditForm));
 
 module.exports = router;
